2b0dc889ad31539dfc669764d64e6caaead9c014
amel-arif <@mel@rif123>
2021-02-22 09:35:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/MediaPitchounesApp/model/PushNotification.kt
@@ -0,0 +1,6 @@
+package com.example.MediaPitchounesApp.model
+
+data class PushNotification(
+    var data:NotificationData,
+    var to:String
+)
---
+++ app/src/main/java/com/example/MediaPitchounesApp/model/User.kt
@@ -0,0 +1,3 @@
+package com.example.MediaPitchounesApp.model
+
+data class User(var userId:String = "", var userName:String = "", var profileImage:String = "")
